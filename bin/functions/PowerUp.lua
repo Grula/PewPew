c68f7d611cf5752b889f6f0f975895d9863e47d8
JelenaZaza <jelenazaza.simovic@gmail.com>
2016-01-06 16:12:59 +0100

    pow

--- bin/functions/PowerUp.lua
+++ bin/functions/PowerUp.lua
@@ -1,9 +1,9 @@
 -- Creates powerUp
 function powerUp( dt )
-	i = math.random(4)
-	posX = math.random(10, love.graphics.getWidth() - player.img:getWidth()/2)
+	i = math.random(5)
+	posX = math.random(10 , love.graphics.getWidth() - player.img:getWidth()/ 2)
 	powerUpImg = imagesPowerup[i]
 	print(powerUpImg)	
-	newPowerUp = {x=posX,y = -10,img = powerUpImg}
+	newPowerUp = {x=posX,y =-10 ,img = powerUpImg}
 	table.insert(activePowerupOnScreen,newPowerUp)
 end
